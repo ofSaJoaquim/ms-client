853cf8b4151aa7aa47e09041510315cfe337b767
desenv03 <desenv03.tb@branetlogistica.com.br>
2023-04-17 16:08:26 -0300

    first commit

---
+++ src/test/java/br/com/branetlogistica/msclient/MsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.branetlogistica.msclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
